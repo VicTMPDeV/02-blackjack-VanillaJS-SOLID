e3863d477f411b5208e2cc347c62b9a923f74476
Victor_Morales_Perez <vmorales@atsistemas.com>
2022-05-09 13:43:59 +0200

    refactor

--- assets/js/juegoApp.js
+++ assets/js/juegoApp.js
@@ -8,178 +8,168 @@
  * New Game / Deal = Nuevo Juego / Repartir
  */
 
-//Global Variables
-let deck           = [];
-let deckSize       = 0;
-const types        = ['C','D','H','S'];
-const minNumCard   = 2;
-const maxNumCard   = 10;
-const specialCards = ['A','J','Q','K'];
-const AceValue     = 11;
-const jqkValue     = 10;
-let playerScore = 0 , computerScore = 0;
+/* 
+PATRÓN DE DISEÑO MODULE - Desde ES5 
+-----------------------------------
+La idea es encapsular el código para que no sea accesible desde el objeto Window
+y que un jugador pueda hacer trampas desde la consola del navegador.
 
-//DOM Ref Variables
-const btnHit     = document.querySelector('#btnHitCard');
-const btnStand   = document.querySelector('#btnStand');
-const btnNewGame = document.querySelector('#btnNewGame');
+Para ello vamos a encapsular el código creando Prototipos usando el patrón módulo
+*/
 
-const playerScoreTag   = document.querySelector('#playerScoreTag'); 
-const computerScoreTag = document.querySelector('#computerScoreTag');
+/*
+MÓDULO : Función que crea una Encapsulación (Scope (Prototipo)), anónima para 
+que no pueda ser referenciado (no tenga un nombre que establezca la ubicación 
+en memoria) el prototipo desde consola y autoinvocada para que se lance nada
+más comenzar
+*/
+(() => {
+    'use strict'
+    //Global Variables
+    let deck = [];
+    const types = ['C', 'D', 'H', 'S'],
+          minNumCard = 2,
+          maxNumCard = 10,
+          specialCards = ['A', 'J', 'Q', 'K'],
+          AceValue = 11,
+          jqkValue = 10;
 
-const divPlayerCards   = document.querySelector('#player-cards'); 
-const divComputerCards = document.querySelector('#computer-cards'); 
+    let scores = [];
 
-createDeck();
-//ESTO FUNCIONA GRACIAS AL HOISTING DE JS
-function createDeck () {
-    //Purgo el deck
-    deck = [];
-    /*Podría renombrar las cartas del 1 al 13
-    para que fuera todo en un único bucle más 
-    sencillo, pero por fines didácticos, quiero 
-    jugar con el segundo bucle for para ver 
-    otra manera de hacerlo.*/
-    for (let i=minNumCard; i<=maxNumCard; i++) {
+    //DOM Ref Variables
+    const btnHit     = document.querySelector('#btnHitCard'),
+          btnStand   = document.querySelector('#btnStand'),
+          btnNewGame = document.querySelector('#btnNewGame');
+
+    const divPlayersCards = document.querySelectorAll('.divCards'),
+          scoreTag = document.querySelectorAll('small');
+
+    // Initializer
+    const initGame = ( numPlayers = 2 ) => {
+        deck = createDeck();
+        scores = [];
+
+        for (let i=0; i<numPlayers; i++) {
+            scores.push(0);          
+        }
+
+        scoreTag.forEach( elem => elem.innerText = 0 );
+        divPlayersCards.forEach( elem => elem.innerHTML = '');
+
+        btnHit.disabled = false;
+        btnStand.disabled = false;
+    }
+
+    const createDeck = () => {
+        deck = [];
+        for (let i = minNumCard; i <= maxNumCard; i++) {
+            for (let type of types) {
+                deck.push(i + type);
+            }
+        }
         for (let type of types) {
-            deck.push(i + type);
+            for (let special of specialCards) {
+                deck.push(special + type);
+            }
         }
+        return _.shuffle(deck);//underscore function
     }
-    for (let type of types) {
-        for (let special of specialCards) {
-            deck.push(special + type);
+
+    const hitCard = () => {
+        if (deck.length === 0) {
+            throw 'No quedan más cartas que jugar';
         }
-    }
-    deck = _.shuffle(deck);//underscore function
-    deckSize = deck.length;
-}
-
-const hitCard = () => {
-    
-    if(deck.length === 0){
-        throw 'No quedan más cartas que jugar';
-    }
-    const card = deck.pop();
-    return card;
-}
-
-// Test de la función hitCard
-// for (let i = 0; i <= deckSize; i++) {
-//     hitCard();
-// }
-
-const cardValue = (card) => {
-
-    const value = card.substring(0, card.length - 1);
-    return (isNaN(value)) ? ( (value === 'A') ? AceValue : jqkValue ) : parseInt(value);
-    /* Legacy Code
-    if( isNaN(value) ){ 
-        //Los únicos valores posibles son 'A','J','Q' y 'K'
-        //En Blackjack 'A' es la única que vale 11 puntos
-        points = (value === 'A') ? 11 : 10; 
-    }else{
-        //El valor numérico es un string, lo convierto a number
-        points = parseInt(value);
-    }
-    */
-}
-
-// Eventos (Callback is present... warning)
-btnNewGame.addEventListener('click', () => {
-
-    console.clear();
-    createDeck();
-
-    playerScore   = 0;
-    computerScore = 0;
-
-    playerScoreTag.innerHTML   = 0;
-    computerScoreTag.innerHTML = 0;
-
-    divPlayerCards.innerHTML   = '';
-    divComputerCards.innerHTML = '';
-
-    btnHit.disabled   = false;
-    btnStand.disabled = false;
-
-});
-
-btnHit.addEventListener('click', () => {
-
-    const hittedCard = hitCard();
-    playerScore = playerScore + cardValue(hittedCard);
-    playerScoreTag.innerHTML = playerScore;
-
-    //<img class="deck-card" src="assets/cards/2C.png">
-    const imgCard = document.createElement('img');
-    imgCard.src = `assets/cards/${hittedCard}.png`;
-    imgCard.classList.add('deck-card')
-    divPlayerCards.append(imgCard);
-
-    if(playerScore > 21){
-        console.warn('HAS PERDIDO');
-        btnHit.disabled   = true;
-        btnStand.disabled = true;
-        ComputerPlay(playerScore);
-    } else if (playerScore === 21) {
-        console.warn('JUGADOR: 21 PUNTOS!!!');
-        btnHit.disabled   = true;
-        btnStand.disabled = true;
-        ComputerPlay(playerScore);
+        return deck.pop();
     }
 
-});
+    const cardValue = (card) => {
+        const value = card.substring(0, card.length - 1);
+        return (isNaN(value)) ? 
+                ((value === 'A') ? AceValue : jqkValue) 
+                : parseInt(value);
+    }
 
-btnStand.addEventListener('click',() => {
-    //Bloqueo las acciones del jugador
-    btnHit.disabled   = true;
-    btnStand.disabled = true;
-    //Juega la máquina
-    ComputerPlay(playerScore);
-});
+    //Turn: 0 = Player - last = Computer
+    const scoring = ( card, turn ) => {
+        scores[turn] = scores[turn] + cardValue(card);
+        scoreTag[turn].innerHTML = scores[turn];
+        return scores[turn];
+    }
 
-//Machine (Computer player) logic -> minScoreToWin = playerScore
-const ComputerPlay = (minScoreToWin) => {
+    const theWinnerIs = () => {
 
-    /*El jugador, como mínimo, pedirá 1 o 0 cartas antes de plantarse
-    , por tanto, la máquina para ganar tendrá que pedir 1 carta por lo 
-    menos */
-    while( (computerScore < minScoreToWin) && (minScoreToWin <= 21) ) {
+        const [playerScore,computerScore] = scores;
 
-        const hittedCard = hitCard();
-        computerScore = computerScore + cardValue(hittedCard);
-        computerScoreTag.innerHTML = computerScore;
+        setTimeout(() => {
+            if (computerScore === playerScore) {
+                alert('EMPATE');
+            } else if (playerScore > 21) {
+                alert('COMPUTADORA GANA');
+            } else if (computerScore > 21) {
+                alert('GANASTE!!!');
+            } else if (playerScore > computerScore) {
+                alert('GANASTE!!!');
+            } else if (computerScore > playerScore) {
+                alert('COMPUTADORA GANA');
+            } else {
+                alert('COMPUTADORA GANA');
+            }
+        }, 100);
+    }
 
-        //<img class="deck-card" src="assets/cards/2C.png">
+    const createCardImage = ( card, turn ) => {
         const imgCard = document.createElement('img');
-        imgCard.src = `assets/cards/${hittedCard}.png`;
-        imgCard.classList.add('deck-card')
-        divComputerCards.append(imgCard);
+        imgCard.src = `assets/cards/${card}.png`;
+        imgCard.classList.add('deck-card');
+        divPlayersCards[turn].append(imgCard);
+    }
 
-        if (minScoreToWin > 21) {
-            break;
-        } 
+    const ComputerPlay = (minScoreToWin) => {
+
+        let computerScore = 0;
+        while ((computerScore < minScoreToWin) && (minScoreToWin <= 21)) {
+            const hittedCard = hitCard();
+            computerScore = scoring(hittedCard, scores.length - 1);
+            createCardImage( hittedCard , scores.length - 1);
+        }
+        theWinnerIs();
 
     }
 
-    setTimeout(() => {
-        if ( computerScore === minScoreToWin ){
-            alert('EMPATE');
-        } else if ( minScoreToWin > 21){
-            alert('COMPUTADORA GANA');
-        } else if (computerScore > 21){
-            alert('GANASTE!!!');
-        } else if (playerScore > computerScore) {
-            alert('GANASTE!!!');
-        } else if (computerScore > playerScore) {
-            alert('COMPUTADORA GANA');
-        } else {
-            alert('COMPUTADORA GANA');
+    // Eventos (Callback is present... warning)
+    btnNewGame.addEventListener('click', () => {
+        initGame();
+    });
+
+    btnHit.addEventListener('click', () => {
+
+        const card = hitCard();
+        const playerScore = scoring( card, 0 );
+
+        createCardImage(card,0);
+
+        if (playerScore > 21) {
+            console.warn('HAS PERDIDO');
+            btnHit.disabled = true;
+            btnStand.disabled = true;
+            ComputerPlay(playerScore);
+        } else if (playerScore === 21) {
+            console.warn('JUGADOR: 21 PUNTOS!!!');
+            btnHit.disabled = true;
+            btnStand.disabled = true;
+            ComputerPlay(playerScore);
         }
-    }, 100);
+
+    });
+
+    btnStand.addEventListener('click', () => {
+        //Bloqueo las acciones del jugador
+        btnHit.disabled = true;
+        btnStand.disabled = true;
+        //Juega la máquina
+        ComputerPlay(scores[0]);
+    });
+
     
 
-}
-//Testing Machine Logic
-// console.log(`puntos jugador ${12}`)
-// ComputerPlay(12);
+})();
